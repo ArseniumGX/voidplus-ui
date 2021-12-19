cfd9eea0fb35c09f455abdaf9242c46f09f1946c
José Pereira Macedo <arseniumgx@gmail.com>
2021-12-18 21:23:31 -0300

    Refact button component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,12 +1,10 @@
-import style from './button.module.scss'
+import style from './Button.module.scss'
 
 function Button(props) {
    return (
       <>
-         <div className={style.ctx}>
-            <button onClick={props.onClick} onSubmit={props.onSubmit}>
-               {props.value}
-            </button>
+         <div className={style.button}>
+            <button onClick={props.onClick}>{props.value}</button>
          </div>
       </>
    )
