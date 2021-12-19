640c66ccee37cb5e652f3235b0f34abb366a6947
José Pereira Macedo <arseniumgx@gmail.com>
2021-12-18 21:26:40 -0300

    Finish component header with menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,14 @@
 import { Menu } from './Menu/Menu'
+import style from './Header.module.scss'
+import logo from '../../assets/voidplus-logo.png'
 
 function Header() {
    return (
-      <header>
-         <h1>VoidPlus</h1>
+      <header className={style.header}>
+         <div className={style.header__logo}>
+            <img src={logo} alt="Voidplus logo" />
+            <h1>VoidPlus</h1>
+         </div>
          <Menu />
       </header>
    )
--- src/components/Header/Menu/Menu.jsx
+++ src/components/Header/Menu/Menu.jsx
@@ -1,15 +1,16 @@
 import { Link } from 'react-router-dom'
+import style from './Menu.module.scss'
 
 function Menu() {
    return (
       <>
-         <nav>
+         <nav className={style.navbar}>
             <ul>
                <li>
                   <Link to="/">Home</Link>
                </li>
                <li>
-                  <Link to="signup">Cadastro</Link>
+                  <Link to="login">Login</Link>
                </li>
                <li>
                   <Link to="sobre">Sobre</Link>
