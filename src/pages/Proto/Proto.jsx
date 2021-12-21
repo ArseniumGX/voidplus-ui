b58499f646d66801801633222a16c60d952527a4
José Pereira Macedo <arseniumgx@gmail.com>
2021-12-21 03:38:28 -0300

    Easteregg

--- src/pages/Proto/Proto.jsx
+++ src/pages/Proto/Proto.jsx
@@ -1,9 +1,20 @@
 import style from './Proto.module.scss'
+import { GiExitDoor } from 'react-icons/gi'
+import { useState } from 'react'
 
 function Proto() {
+   const token = localStorage.getItem('token')
+   const [state, setState] = useState(false)
+
+   const handleLogin = () => {
+      localStorage.removeItem('token')
+      setState(!state)
+   }
+
    return (
       <section className={style.proto}>
          <h2>Página de testes</h2>
+         {!!token ? <GiExitDoor onClick={handleLogin} /> : 'fazerlogin'}
       </section>
    )
 }
